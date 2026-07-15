67fabde122a2c431d65da3b3a4a9af74f5a27801
aajin2837-source <aajin2837@gamil.com>
2026-07-15 10:01:38 +0530

    python anywere backend settings url updated

--- backpage/backpage/urls.py
+++ backpage/backpage/urls.py
@@ -19,11 +19,12 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('tiva.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
    
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
